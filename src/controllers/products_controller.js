92bb17a2dfc2aaba6db52fd1d9ebab9d3083c52c
MURILO LOURENCO CABRAL <murilolcabral@gmail.com>
2022-07-06 11:45:19 -0300

    Server/Databse/Model/Controller adicionado

--- src/controllers/products_controller.js
+++ src/controllers/products_controller.js
@@ -2,7 +2,12 @@ const ProductsModel = require('../models/products')
 
 async function get(req, res){
 
-    const products = await ProductsModel.find()
+    const { id } = req.params
+
+    let obj = id ? { _id: id} : null
+
+    
+    const products = await ProductsModel.find(obj)
 
     res.send(products)
 }
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -2,7 +2,7 @@ const router = require('express').Router()
 
 const ProductControlle = require('../controllers/products_controller')
 
-router.get('/products', ProductControlle.get)
+router.get('/products/:id?', ProductControlle.get)
 // router.post('/products', ProductControlle.post)
 // router.put('/products/:id', ProductControlle.put)
 // router.delete('/products/:id', ProductControlle.delete)
